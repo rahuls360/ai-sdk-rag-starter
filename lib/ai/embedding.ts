d4e85eebc40e8d2f8da2256109ae9b04d53d048e
Rahul Makhija <rahuls360@gmail.com>
2025-02-14 15:27:40 +0530

    add logs

--- lib/ai/embedding.ts
+++ lib/ai/embedding.ts
@@ -1,49 +1,55 @@
-import { embed, embedMany } from 'ai';
-import { openai } from '@ai-sdk/openai';
-import { db } from '../db';
-import { cosineDistance, desc, gt, sql } from 'drizzle-orm';
-import { embeddings } from '../db/schema/embeddings';
+import { embed, embedMany } from "ai";
+import { openai } from "@ai-sdk/openai";
+import { db } from "../db";
+import { cosineDistance, desc, gt, sql } from "drizzle-orm";
+import { embeddings } from "../db/schema/embeddings";
 
-const embeddingModel = openai.embedding('text-embedding-ada-002');
+const embeddingModel = openai.embedding("text-embedding-ada-002");
 
 const generateChunks = (input: string): string[] => {
-    return input
-        .trim()
-        .split('.')
-        .filter(i => i !== '');
+	return input
+		.trim()
+		.split(".")
+		.filter((i) => i !== "");
 };
 
 export const generateEmbeddings = async (
-    value: string,
+	value: string
 ): Promise<Array<{ embedding: number[]; content: string }>> => {
-    const chunks = generateChunks(value);
-    const { embeddings } = await embedMany({
-        model: embeddingModel,
-        values: chunks,
-    });
-    return embeddings.map((e, i) => ({ content: chunks[i], embedding: e }));
+	console.log("Generating embeddings for:", value);
+	const chunks = generateChunks(value);
+	console.log("Chunks:", chunks);
+	const { embeddings } = await embedMany({
+		model: embeddingModel,
+		values: chunks,
+	});
+	console.log("Embeddings:", embeddings);
+	return embeddings.map((e, i) => ({ content: chunks[i], embedding: e }));
 };
 
 export const generateEmbedding = async (value: string): Promise<number[]> => {
-    const input = value.replaceAll('\\n', ' ');
-    const { embedding } = await embed({
-        model: embeddingModel,
-        value: input,
-    });
-    return embedding;
+	const input = value.replaceAll("\\n", " ");
+	const { embedding } = await embed({
+		model: embeddingModel,
+		value: input,
+	});
+	return embedding;
 };
 
 export const findRelevantContent = async (userQuery: string) => {
-    const userQueryEmbedded = await generateEmbedding(userQuery);
-    const similarity = sql<number>`1 - (${cosineDistance(
-        embeddings.embedding,
-        userQueryEmbedded,
-    )})`;
-    const similarGuides = await db
-        .select({ name: embeddings.content, similarity })
-        .from(embeddings)
-        .where(gt(similarity, 0.5))
-        .orderBy(t => desc(t.similarity))
-        .limit(4);
-    return similarGuides;
+	const userQueryEmbedded = await generateEmbedding(userQuery);
+	console.log("User query embedded:", userQueryEmbedded);
+	const similarity = sql<number>`1 - (${cosineDistance(
+		embeddings.embedding,
+		userQueryEmbedded
+	)})`;
+	console.log("Similarity consine distance:", similarity);
+	const similarGuides = await db
+		.select({ name: embeddings.content, similarity })
+		.from(embeddings)
+		.where(gt(similarity, 0.5))
+		.orderBy((t) => desc(t.similarity))
+		.limit(4);
+	console.log("Similar guides:", similarGuides);
+	return similarGuides;
 };
